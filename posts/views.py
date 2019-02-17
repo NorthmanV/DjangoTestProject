5f50a961cc253dbec1e3910a2656d52125097fab
Ruslan Akberov <ruslanakberov@gmail.com>
2019-02-17 16:06:09 +0300

    Posts model created

--- posts/views.py
+++ posts/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def index(request):
-    return HttpResponse('Hello from posts')
+    #return HttpResponse('Hello from posts')
+    return render(request, 'posts/index.html', {'title': 'Latest Posts'})
 
